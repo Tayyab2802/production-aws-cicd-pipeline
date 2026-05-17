936ce6bdd926b29b0c33e9b0a29e915c5d02c1c8
Tayyab2802 <tayyabkhalid024@gmail.com>
2026-05-17 23:42:54 +0100

    Created dev environment network foundation

---
+++ infra/environments/dev/main.tf
@@ -0,0 +1,18 @@
+module "network" {
+  source = "../../modules/network"
+
+  project_name = "production-pipeline"
+  environment  = "dev"
+
+  vpc_cidr = "10.0.0.0/16"
+
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  availability_zones = [
+    "eu-west-2a",
+    "eu-west-2b"
+  ]
+}
---
+++ infra/modules/network/main.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  description = "Name of the project."
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (dev or prod)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets."
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones for public subnets."
+  type        = list(string)
+}
